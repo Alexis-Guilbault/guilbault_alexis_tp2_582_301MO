538cc1800ef99bd73846408d810b1c20c228ce6f
Alexis <alexisguilbault@outlook.com>
2025-09-27 10:45:20 -0400

    Test commit
    Exemple pour un ami

--- guilbault_alexis_tp2/scenes/objets/epine.gd
+++ guilbault_alexis_tp2/scenes/objets/epine.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
+func _ready() -> void:
+	$Area2D.body_entered.connect(Callable(self, "_on_area_2d_body_entered"))
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+func _on_area_2d_body_entered(body: Node) -> void:
+	print("Body entered:", body.name)
