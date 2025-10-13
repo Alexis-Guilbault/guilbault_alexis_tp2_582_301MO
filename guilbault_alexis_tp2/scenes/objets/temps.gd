bda720634a0d66afbef37d4b6d5721c561d13687
Alexis <alexisguilbault@outlook.com>
2025-10-13 12:36:39 -0400

    Fin TP2

--- guilbault_alexis_tp2/scenes/objets/temps.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-
-var temps = 0
-
-func _process(_delta: float) -> void:
-	temps += 1
-	text = "temps : " + var_to_str(floor(temps/60))
--- guilbault_alexis_tp2/scenes/objets/texte_boost.gd
+++
@@ -1,8 +0,0 @@
-extends Label
-
-func _process(_delta: float) -> void:
-	match $"../../../joueur".boostitem:
-		true:
-			text = "BOOST !!"
-		false:
-			text = " "
